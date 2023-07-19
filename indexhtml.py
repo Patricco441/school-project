2650f76351d164ca6e307aba745a9dde4ae4c3e9
Patryk Hachamowski <patryk.hachamowski@asseco.pl>
2023-07-19 15:43:55 +0200

    first commit

---
+++ indexhtml.py
@@ -0,0 +1,21 @@
+css='''
+<style>
+background-color: #f5f5f5;
+
+</style>
+
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+
+</div>
+<div class=message>{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+<div class=message>{{MSG}}</div>
+</div>
+'''
